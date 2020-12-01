3162de306ddf1e6be77c0e1bd339069b8ba5c50c
Shay EK <shayek34@gmail.com>
2020-12-01 17:28:58 +0200

    Shay push 2

--- main.py
+++ main.py
@@ -1,25 +1,9 @@
 import time
 from myfrontend import *
-from myfrontend import *
 
 import kivy
 import mybackend
 import myfrontend
-
-# class MyApp(App):
-#     def build(self):
-#         return Label(text="Our first app")
-class MyApp(App):
-    def build(self):
-        return Label(text="Our first app")
-
-
-
-
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
 def print_hi(name):
@@ -29,9 +13,9 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    MyApp().run()
+    # MyApp().run()
     # time_start_logreg = time.time()
-    # db = mybackend.Database()
+    db = mybackend.Database()
+    db.calculate_res("City Hall",3,4)
     # print(db)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- mybackend.py
+++ mybackend.py
@@ -1,8 +1,10 @@
 import csv
+import operator
 import sqlite3
 import pandas as pd
 import numpy as np
 import datetime
+
 
 class Database:
     def __init__(self):
@@ -18,7 +20,11 @@ class Database:
             " EndStationLongitude REAL, BikeID INTEGER, UserType TEXT, BirthYear INTEGER, Gender"
             " INTEGER, TripDurationinmin INTEGER)")
         self.conn.commit()
+        num_records = self.check_record_num()
+        if num_records == 0:
+            self.insert_values()
 
+    def insert_values(self):
         with open('BikeShare.csv', 'r') as f:
             reader = csv.reader(f)
             data = next(reader)
@@ -30,12 +36,68 @@ class Database:
             self.conn.commit()
 
         # execute the search query on the DB.
-    def execute_query(self, start_station_name, time_duration):
+
+    def execute_search_query(self, start_station_name, time_duration):
         query = "SELECT * FROM bikeShare WHERE StartStationName = '" + start_station_name + "' AND TripDurationinmin <= '" + str(time_duration) + "'"
+        results = self.cursor.execute(query)
+        records = results.fetchall()
+        cols = [column[0] for column in results.description]
+        df = pd.DataFrame.from_records(data=records, columns=cols)
+        print("1: " + str(df.shape[0]))
+        return df
+
+    def extract_end_stations(self, start_station_name, time_duration):
+        query = "SELECT DISTINCT EndStationName FROM bikeShare WHERE StartStationName = '" + start_station_name + "' AND TripDurationinmin <= '" + str(
+            time_duration) + "'"
         results = self.cursor.execute(query)
         cols = [column[0] for column in results.description]
         df = pd.DataFrame.from_records(data=results.fetchall(), columns=cols)
         return df
 
+    def calculate_res(self, start_station_name, time_duration, num):
+        all_values = self.extract_all_data()
+        end_stations = self.extract_end_stations(start_station_name, time_duration)
+        # all_filterd_results = self.execute_search_query(start_station_name, time_duration)
+        dict = self.count_end_stations(end_stations, all_values)
+        print(dict)
+        dict2 = self.count_trip_ways(start_station_name,end_stations, all_values)
+        print(dict2)
 
 
+    def count_end_stations(self, end_stations, all_first_results):
+        end_stations_count = {}
+        for station in end_stations.values:
+            station_val = station[0]
+            seriesObj = all_first_results.apply(lambda x: True if x['EndStationName'] == station_val else False, axis=1)
+            # Count number of True in series
+            numOfRows = len(seriesObj[seriesObj == True].index)
+            end_stations_count[station_val] = numOfRows
+        sorted_end_stations_count = dict(sorted(end_stations_count.items(),key=operator.itemgetter(1), reverse=True))
+        return sorted_end_stations_count
+
+    def check_record_num(self):
+        query = "SELECT * FROM bikeShare"
+        results = self.cursor.execute(query)
+        records = results.fetchall()
+        cols = [column[0] for column in results.description]
+        df = pd.DataFrame.from_records(data=records, columns=cols)
+        return df.shape[0]
+
+    def count_trip_ways(self, start_station_name, end_stations, all_first_results):
+        trip_count = {}
+        for station in end_stations.values:
+            station_val = station[0]
+            seriesObj = all_first_results.apply(lambda x: True if x['StartStationName'] == start_station_name and x['EndStationName'] == station_val else False, axis=1)
+            # Count number of True in series
+            numOfRows = len(seriesObj[seriesObj == True].index)
+            trip_count[station_val] = numOfRows
+        sorted_trip_count = dict(sorted(trip_count.items(), key=operator.itemgetter(1), reverse=True))
+        return sorted_trip_count
+
+    def extract_all_data(self):
+        query = "SELECT * FROM bikeShare"
+        results = self.cursor.execute(query)
+        records = results.fetchall()
+        cols = [column[0] for column in results.description]
+        all_data = pd.DataFrame.from_records(data=records, columns=cols)
+        return all_data
